d88502f1825d621d3bb3f1e2b322682969bfcfa1
Salvatore Miceli <salvatore.miceli@nextup.cloud>
2020-12-14 10:03:30 +0100

    feat: php8

--- bin/apache2_multiphp/start.sh
+++ bin/apache2_multiphp/start.sh
@@ -2,13 +2,16 @@
 sed -i 's/user = www-data/user = www/g' /etc/php/5.6/fpm/pool.d/www.conf
 sed -i 's/user = www-data/user = www/g' /etc/php/7.2/fpm/pool.d/www.conf
 sed -i 's/user = www-data/user = www/g' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i 's/user = www-data/user = www/g' /etc/php/8.0/fpm/pool.d/www.conf
 sed -i 's/;catch_workers_output = yes/catch_workers_output = yes/g' /etc/php/5.6/fpm/pool.d/www.conf
 sed -i 's/;catch_workers_output = yes/catch_workers_output = yes/g' /etc/php/7.2/fpm/pool.d/www.conf
 sed -i 's/;catch_workers_output = yes/catch_workers_output = yes/g' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i 's/;catch_workers_output = yes/catch_workers_output = yes/g' /etc/php/8.0/fpm/pool.d/www.conf
 sed -i 's/export APACHE_RUN_USER=www-data/export APACHE_RUN_USER=www/g' /etc/apache2/envvars
 /usr/sbin/service php5.6-fpm start
 /usr/sbin/service php7.2-fpm start
 /usr/sbin/service php7.4-fpm start
+/usr/sbin/service php8.0-fpm start
 /usr/sbin/service apache2 start
 sudo -H sh -c "git config --global credential.helper store;
 cat > /root/.git-credentials <<EOF
