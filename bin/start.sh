5fb13035be709c0599522fd0f4b15bea4c0fd673
Salvatore Miceli <salvatore.miceli@nextup.cloud>
2021-04-09 12:22:13 +0200

    feat: add real path

--- bin/start.sh
+++ bin/start.sh
@@ -1,1 +1,2 @@
-docker-compose -f "docker-compose.yml" up -d --build
+SCRIPTPATH="$( cd -- "$(dirname $(dirname "$0"))" >/dev/null 2>&1 ; pwd -P )"
+docker-compose -f "$SCRIPTPATH/docker-compose.yml" up -d --build
--- bin/stop.sh
+++ bin/stop.sh
@@ -1,1 +1,2 @@
-docker-compose -f "docker-compose.yml" down -v
+SCRIPTPATH="$( cd -- "$(dirname $(dirname "$0"))" >/dev/null 2>&1 ; pwd -P )"
+docker-compose -f "$SCRIPTPATH/docker-compose.yml" down
